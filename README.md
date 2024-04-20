99c41bb132a5905cbc23b4b3088d7294756e7392
Richard <rlar0203@tamu.edu>
2024-04-19 19:31:06 -0500

    finish raytracer sphere and phong shader

---
+++ README.md
@@ -0,0 +1,13 @@
+
+        //COMPLETED
+        -camera setup/ rays 
+        -plane class and raytracer
+       
+        // TODO FINISH ELLIPSOID/SPHERE RAY TRACER THEN DO THE PHONG FUNCTION 
+        // - finsih calculations in ray tracer 
+        // - set up phong function and possible pixel coloring function where we determine the color and pixel 
+
+        // - set up 1st scene and test 
+
+        // add shadow tracer and set it up to be recursively searching for multiple lights 
+        // set up reflection capabilities in the ray tracer so it can recursively ray trace
--- src/Rays.h
+++ src/Rays.h
@@ -27,7 +27,6 @@ vector<vec3> Ray_gen(vec3 camera,vec3 plane_pos,int height,int width){
 
 //makes empty vectors for all the possible rays avaialble 
 vector<vec3> Rays (max_dimension*max_dimension, vec3(0.0,0.0,0.0));
-   
     //calculates 45 degrees in radians
   double   FOV_y = M_PI*((double)45/(double)180);
     
@@ -36,6 +35,7 @@ vector<vec3> Rays (max_dimension*max_dimension, vec3(0.0,0.0,0.0));
 
     //gets the dimension of the grid side 
    double  grid_size = 2.0 *( plane_dist * tan(FOV_y/2));
+
 
     //gets the pixel dimensions 
    double pixel_size =  grid_size / std::max(height,width); 
@@ -46,6 +46,7 @@ vector<vec3> Rays (max_dimension*max_dimension, vec3(0.0,0.0,0.0));
        for(int j = 0; j < max_dimension; j++){
         //gets the x position starting at the left and then going right 
         Rays.at(max_dimension*i +j).x = pixel_size * ( j - (max_dimension/2.0) + 0.5) ;
+
 
         //makes this negative so the starting point is now the top left 
         Rays.at(max_dimension*i +j).y = -pixel_size * (i - (max_dimension/2.0) + 0.5) ;
@@ -67,38 +68,111 @@ vector<vec3> Rays (max_dimension*max_dimension, vec3(0.0,0.0,0.0));
 }
 
 
+
 //sets up the parent class which has the children classes inheriting from the parent 
     class Object
     {
-    public:
-        virtual ~Object() {};
+    protected:
+        vec3 point;
+        //color values for the object
 
-        virtual double ray_trace() const = 0;
+        
+    public:        
+        vec3 kd;
+        vec3 ks;
+        vec3 ka;
+        double s;
+    //basic constructor for the object so we can all the constructor inside the subshape constructors
+        Object(vec3 point,vec3 kd, vec3 ka, vec3 ks, double s): point(point) {};
+        virtual ~Object() {};
     };
     
 
     
 
 
-    class Ellipsoid : private Object
+    class Ellipsoid : public Object
     {
+        private:
+        //assumes radius are equal when working in local space so they will be the same 
+        double radius = 1.00;
         
+        //transformation matrix  goint from local to world
+        mat4 E(1.0); //automatically identity 
+
+        public:
+        //assumes no scale and a rotation of zero around the xy axis 
+        Ellipsoid( vec3 point, vec3 kd, vec3 ka, vec3 ks, double s, vec3 SCALE = vec3(1.00,1.00,1.00), vec3 Rotation_axis = vec3(1,1,0), double rotation_angle = 0 ): Object(point,kd,ka,ks,s){
+            //applies the transformations and stores them in E matrix
+            E = scale(E, SCALE);
+            E = rotate(E,rotation_angle,Rotation_axis);
+
+        }
+
+        vector<vec3> single_raytrace(vec3 Ray_dir, vec3 Ray_origin){
+            //transforms the ray vectors into local space (if it is an identiy matrix will remain the same)
+            vec3 T_raydir = (inverse(E) * vec4(Ray_dir, 0.0)).xyz;
+            vec3 T_rayorig = (inverse(E) * vec4(Ray_origin, 0.0)).xyz;
+
+            vector<vec3> Results(3,vec3(0.0,0.0,0.0));
+
+        //storing values to be used in calculating t 
+        double a = dot(T_raydir , T_rayorig);
+        double b = 2* dot(v,(T_rayorig - point));
+        double d = (b*b) - (4*a*());
+
+
+        // TODO FINISH ELLIPSOID/SPHERE RAY TRACER THEN DO THE PHONG FUNCTION 
+        // - finsih calculations in ray tracer 
+        // - 
+        if()
+
+
+
+            return Results;
+        }
 
 
     }
 
-    class plane : private Object
+    class plane : public Object
     {
     private:
-        /* data */
+    vec3 normal;
     public:
-        plane(/* args */);
-        ~plane();
+    // creates plane and initalizes values in parent 
+        plane(vec3 normal, vec3 point, vec3 kd, vec3 ka, vec3 ks, double s): Object(point,kd,ka,ks,s){
+            this->normal = normal;
+        };
+        //does ray_tracing for a singluar ray and returns the hit position, hit normal and the hit distance 
+        vector<vec3> single_raytrace( vec3 Ray_dir, vec3 Ray_origin){
+        //results which include 1 distance value stored in x position in a vec3 and  two vec3 for point hit and hit normal
+        vector<vec3> Results(3,vec3(0.0,0.0,0.0));
+
+        //checks if the ray is parrallel to plane to avoid trying to divide by zero just in case 
+        double angle_ray_plane = dot(normal,Ray_dir);
+
+        if(angle_ray_plane > 0.00000000000001 || angle_ray_plane < -0.00000000000001){
+            //calculates t value
+            Results.at(0).x = (dot(normal, point) - dot(normal,Ray_origin) ) / dot(normal,Ray_dir);
+
+            //calculates the hit position
+            Results.at(1) = Ray_origin + (Ray_dir * Results.at(0).x);
+
+            //since every point on the plane has the same normal so hit normal = plane normal
+            Results.at(2) = normal;
+        }
+        //returns all negative distance vals in vec3 meaning ray can't hit plane
+        else{
+            Results.at(0) = vec3(-1,-1,-1);
+        }
+
+
+            return Results;
+        };
+        
     };
-    
-    plane::plane(/* args */)
-    {
-    }
+
     
 
     
